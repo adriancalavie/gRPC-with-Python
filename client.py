7456d8b9fa63b821f116ae04e413de2710b4910f
Adrian Calavie <adrian.calavie06@gmail.com>
2021-03-23 15:39:37 +0200

    Removed unused commented run() function

--- client.py
+++ client.py
@@ -8,14 +8,6 @@ import user_pb2
 import user_pb2_grpc
 
 
-# def run():
-#     with grpc.insecure_channel('localhost:50051') as channel:
-#         stub = user_pb2_grpc.UserStub(channel)
-#
-#         response = stub.SubmitData(user_pb2.UserDataRequest(name='Adrian', cnp='225200'))
-#     print("Client received: " + response.response)
-
-
 class Client(object):
 
     def __init__(self):
