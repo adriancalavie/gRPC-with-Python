74c5041cd73c724526aaa32fc5f677f9ee840e13
Adrian Calavie <adrian.calavie06@gmail.com>
2021-03-23 15:37:42 +0200

    Added age, gender logic

--- client.py
+++ client.py
@@ -17,14 +17,23 @@ import user_pb2_grpc
 
 
 class Client(object):
-    __whatever = 12
 
     def __init__(self):
         self.channel = grpc.insecure_channel('localhost:50051')
         self.stub = user_pb2_grpc.UserStub(self.channel)
 
         self.name = input('Enter a name: ')
-        self.cnp = input('Enter a TIN: ')
+
+        is_cnp = False
+
+        while not is_cnp:
+            self.cnp = input('Enter a TIN:')
+
+            if validate(self.cnp):
+                is_cnp = True
+                print('Connected.')
+            else:
+                print('Invalid TIN')
 
     def submit(self):
         response = self.stub.SubmitData(user_pb2.UserDataRequest(name=self.name, cnp=self.cnp))
@@ -36,7 +45,15 @@ def run():
 
     is_connected = True
 
+    options = {
+        1: "1.Send data to the server",
+        2: "2.Disconnect",
+    }
+
     while is_connected:
+        for index in options:
+            print(options[index])
+
         option = input('Enter option: ')
 
         if option is '1':
@@ -49,6 +66,45 @@ def run():
 
         else:
             print('Invalid option. Choose again!')
+
+
+def validate(cnp):
+    CNP_CONSTANT = "279146358279"
+    component = 0
+
+    birth_century = frozenset([1, 2, 5, 6, 7, 8])
+
+    if len(cnp) != 13:
+        return False
+    elif cnp.isnumeric():
+        gender = cnp[0]
+        year = cnp[1:3]
+        month = cnp[3:5]
+        day = cnp[5:7]
+
+        if int(gender) not in birth_century:
+            return False
+
+        if 1 > int(month) > 12:
+            return False
+
+        if 1 > int(day) > 31:
+            return False
+
+        for index in range(len(CNP_CONSTANT)):
+            component += int(CNP_CONSTANT[index]) * int(cnp[index])
+
+        remainder = component % 11
+
+        if remainder == 10:
+            remainder = 1
+
+        if str(remainder).__eq__(cnp[-1]):
+            return True
+        else:
+            return False
+
+    return False
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 from concurrent import futures
 
@@ -9,7 +10,9 @@ import user_pb2_grpc
 
 class Server(user_pb2_grpc.UserServicer):
     def SubmitData(self, request, context):
-        print("Client " + request.name + " has logged in")
+        age = get_age(request.cnp)
+        gender = get_gender(request.cnp)
+        print('Client ' + request.name + f"({gender}, {age} years)" + ' connected.')
         return user_pb2.ServerResponse(response="Submitted.")
 
 
@@ -20,6 +23,39 @@ def serve():
     server.start()
     print('Server started')
     server.wait_for_termination()
+
+
+def get_age(cnp):
+    default_year = 1900
+    today_date = datetime.date.today()
+
+    gender = cnp[0]
+    year = cnp[1:3]
+    month = cnp[3:5]
+    day = cnp[5:7]
+
+    period_count = {
+        1: default_year,
+        2: default_year,
+        5: default_year + 100,
+        6: default_year + 100,
+        7: default_year,
+        8: default_year
+
+    }
+
+    client_birth_date = datetime.date(period_count.get(int(gender)) + int(year), int(month), int(day))
+
+    return int((today_date - client_birth_date).days / 365)
+
+
+def get_gender(cnp):
+    gender = cnp[0]
+
+    if int(gender) % 2 == 0:
+        return "female"
+    else:
+        return "male"
 
 
 if __name__ == '__main__':
